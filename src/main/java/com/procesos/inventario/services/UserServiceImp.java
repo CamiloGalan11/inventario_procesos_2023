6bdf54ec8f4674a6ec14b4dddaf9d13dcec367a0
Camilo Galan <cagalano@ufpso.edu.co>
2023-05-30 19:34:04 -0500

    Agregando Security

--- src/main/java/com/procesos/inventario/services/UserServiceImp.java
+++ src/main/java/com/procesos/inventario/services/UserServiceImp.java
@@ -2,16 +2,22 @@ package com.procesos.inventario.services;
 
 import com.procesos.inventario.models.User;
 import com.procesos.inventario.repository.UserRepository;
+import com.procesos.inventario.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServiceImp implements  UserService{
+public class UserServiceImp implements UserService{
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public User getUser(Long id){
         return userRepository.findById(id).get();
@@ -20,11 +26,13 @@ public class UserServiceImp implements  UserService{
     @Override
     public Boolean createUser(User user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
             return true;
         }catch (Exception e){
             return false;
         }
+
     }
 
     @Override
@@ -34,19 +42,35 @@ public class UserServiceImp implements  UserService{
 
     @Override
     public Boolean updateUser(Long id, User user) {
-        try{
-        User userBD = userRepository.findById(id).get();
+        try {
+            User userBD = userRepository.findById(id).get();
 
-        userBD.setFirstName(user.getFirstName());
-        userBD.setLastName(user.getLastName());
-        userBD.setAddress(user.getAddress());
-        userBD.setBirthday(user.getBirthday());
-        User userUp = userRepository.save(userBD);
-        return true;
-    }catch (Exception e){
+            userBD.setFirstName(user.getFirstName());
+            userBD.setLastName(user.getLastName());
+            userBD.setBirthday(user.getBirthday());
+            userBD.setAddress(user.getAddress());
+            userBD.setPassword(passwordEncoder.encode(user.getPassword()));
+            User userUp = userRepository.save(userBD);
+            return true;
+        }catch (Exception e){
             return false;
         }
     }
+
+    @Override
+    public String login(User user) {
+        Optional<User> userBd = userRepository.findByEmail(user.getEmail());
+        if(userBd.isEmpty()){
+            throw new RuntimeException("Usuario no encontrado!");
+        }
+
+        if(!passwordEncoder.matches(user.getPassword(), userBd.get().getPassword())){
+            throw new RuntimeException("La contraseña es incorrecta!");
+        }
+        return jwtUtil.create(String.valueOf(userBd.get().getId()),
+                                String.valueOf(userBd.get().getEmail()));
+    }
+
 }
 
 
